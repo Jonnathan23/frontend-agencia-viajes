ce1e0f4456123fbc1030b401e25441d3fc133646
Jonnathan23 <jsaquicela26@gmail.com>
2025-05-19 23:54:14 -0500

    implementacion del router para clerk

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import Router from './router'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const queryClient = new QueryClient()
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>    
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
-  </StrictMode>,
+    <StrictMode>
+        <ClerkProvider publishableKey={clerkPublishableKey}>
+            <QueryClientProvider client={queryClient}>
+                <Router />
+            </QueryClientProvider>
+        </ClerkProvider>
+    </StrictMode>,
 )
